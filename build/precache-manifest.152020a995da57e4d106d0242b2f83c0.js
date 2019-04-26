cd52e318a5a81bdc26f372bcedf8a7391d4ba2e7
KhoomeiK <rohan.pandey@gmail.com>
2019-04-26 16:10:12 -0700

    build

---
+++ build/precache-manifest.152020a995da57e4d106d0242b2f83c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cefbcb1502da74a4078b",
+    "url": "/static/js/main.f37c7131.chunk.js"
+  },
+  {
+    "revision": "4bf67bfe6f0d3fa46e79",
+    "url": "/static/js/2.ccf6d7e0.chunk.js"
+  },
+  {
+    "revision": "cefbcb1502da74a4078b",
+    "url": "/static/css/main.7aa9f948.chunk.css"
+  },
+  {
+    "revision": "7cba2a259b50b3d8cbe33d203f0028c3",
+    "url": "/index.html"
+  }
+];
